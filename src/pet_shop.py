7b3170dc6bf02b7ed199829cc004a9f31f1a0a9f
Abi Johnson <99146064+AbiJays@users.noreply.github.com>
2022-07-04 13:30:25 +0100

    Finished

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -2,14 +2,15 @@
 def get_pet_shop_name(shop_name):
     return shop_name["name"]
     # return self.cc_pet_shop["name"] 
-        # I dont need to do this because I am not passing in the argument. 
+        # I dont need to specify by typing self.cc_pet_shop because I am not passing in the argument 
         # I am giving the base template so I just put here's that variable i needed to get 
-        #  and this is the key value key to get the right bit
+        #  and this is the key value key to get the right bit.
 
 # 2.
 def get_total_cash(pet_shop):
     return pet_shop["admin"]["total_cash"]
     # breakpoint() is amazing...??? maybe? We'll see
+    # inside 
 
 # 3 & 4.
 def add_or_remove_cash(pet_shop, pet_price):
@@ -17,6 +18,7 @@ def add_or_remove_cash(pet_shop, pet_price):
         pet_shop["admin"]["total_cash"] += pet_price
     else: 
         pet_shop["admin"]["total_cash"] -= pet_price
+    # there is a use case for the if else statement even though it's not necessary for this functionality
 
 # 5.
 def get_pets_sold(pet_shop):
@@ -30,43 +32,69 @@ def increase_pets_sold(pet_shop, sold_pets):
 def get_stock_count(pet_shop):
     return len(pet_shop["pets"])
 
-# # 8 & 9
-# def get_pets_by_breed(pet_shop, searching_breed):
-#     for pet in pet_shop:
-#         if ["breed"] == searching_breed:
-#             return "found" + pet
-#         else:
-#             return "not found"
+# 8 & 9
+def get_pets_by_breed(pet_shop, searching_breed):
+    found_pets = []
+    for pet in pet_shop["pets"]:
+        if pet["breed"] == searching_breed:
+            found_pets.append(pet)
+            # APPEND!!!!! NOT AMEND
+    return found_pets
 
-# def get_pets_by_breed(pet_shop, searching_breed):
-#     pets = []
+# 10 & 11
+# def find_pet_by_name(pet_shop, name):
 #     for pet in pet_shop["pets"]:
-#         if ["breed"] == searching_breed:
-#             pets.amend(pet["name"])
+#         if pet["name"] == name: # you can refer to either searched name or pet["name"] first
+#             return pet
 
-
-# 10.
 def find_pet_by_name(pet_shop, name):
-    for pet in pet_shop:
-        if name == ["name"]:
-            print(pet["name"])
-
-# 11.
+    for pet in pet_shop["pets"]:
+        if pet["name"] == name:
+            return pet
 
 
 # 12.
+def remove_pet_by_name(pet_shop, name):
+    pet_to_delete = find_pet_by_name(pet_shop, name)
+    pet_shop["pets"].remove(pet_to_delete)
+# refer to the previous function because we need that function anyway and we shouldn't assign an additional action to a function. It should just have one.
+
 
 
 # 13.
+def add_pet_to_stock(pet_shop, pet):
+    pet_shop["pets"].append(pet)
 
 
 # 14.
+def get_customer_cash(customer):
+    return customer["cash"]
 
 
 # 15.
+def remove_customer_cash(customer, amount):
+    customer["cash"] -= amount
 
 
 # 16.
+def get_customer_pet_count(customer):
+    return len(customer["pets"])
 
 
 # 17.
+def add_pet_to_customer(customer, pet):
+    customer["pets"].append(pet)
+
+
+# 18, 19, 20
+def customer_can_afford_pet(customer, pet):
+    return customer["cash"] >= pet["price"]
+
+# 21, 22, 23
+def sell_pet_to_customer(pet_shop, pet, customer):
+    if pet != None and customer_can_afford_pet(customer, pet):
+        remove_pet_by_name(pet_shop, pet["name"])
+        add_pet_to_customer(customer, pet)
+        remove_customer_cash(customer, pet["price"])
+        add_or_remove_cash(pet_shop, pet["price"])
+        increase_pets_sold(pet_shop, 1)
--- tests/pet_shop_test.py
+++ tests/pet_shop_test.py
@@ -115,15 +115,15 @@ class TestPetShop(unittest.TestCase):
         count = get_stock_count(self.cc_pet_shop)
         self.assertEqual(6, count)
 
-# # 8.
-#     def test_all_pets_by_breed__found(self):
-#         pets = get_pets_by_breed(self.cc_pet_shop, "British Shorthair")
-#         self.assertEqual(2, len(pets))
+# 8.
+    def test_all_pets_by_breed__found(self):
+        pets = get_pets_by_breed(self.cc_pet_shop, "British Shorthair")
+        self.assertEqual(2, len(pets))
 
-# # 9.
-#     def test_all_pets_by_breed__not_found(self):
-#         pets = get_pets_by_breed(self.cc_pet_shop, "Dalmation")
-#         self.assertEqual(0, len(pets))
+# 9.
+    def test_all_pets_by_breed__not_found(self):
+        pets = get_pets_by_breed(self.cc_pet_shop, "Dalmation")
+        self.assertEqual(0, len(pets))
 
 # 10.
     def test_find_pet_by_name__returns_pet(self):
@@ -131,13 +131,11 @@ class TestPetShop(unittest.TestCase):
         self.assertEqual("Arthur", pet["name"])
 
 # 11.
-    @unittest.skip("delete this line to run the test")
     def test_find_pet_by_name__returns_None(self):
         pet = find_pet_by_name(self.cc_pet_shop, "Fred")
         self.assertIsNone(pet)
 
 # 12.
-    @unittest.skip("delete this line to run the test")
     def test_remove_pet_by_name(self):
         remove_pet_by_name(self.cc_pet_shop, "Arthur")
         pet = find_pet_by_name(self.cc_pet_shop,"Arthur")
@@ -178,28 +176,26 @@ class TestPetShop(unittest.TestCase):
 
     # --- OPTIONAL ---
 
-# 18.
+# 18, 19, 20
     @unittest.skip("delete this line to run the test")
     def test_customer_can_afford_pet__sufficient_funds(self):
         customer = self.customers[0]
         can_buy_pet = customer_can_afford_pet(customer, self.new_pet)
         self.assertEqual(True, can_buy_pet)
 
-# 19.
     @unittest.skip("delete this line to run the test")
     def test_customer_can_afford_pet__insufficient_funds(self):
         customer = self.customers[1]
         can_buy_pet = customer_can_afford_pet(customer, self.new_pet)
         self.assertEqual(False, can_buy_pet)
 
-# 20.
     @unittest.skip("delete this line to run the test")
     def test_customer_can_afford_pet__exact_funds(self):
         customer = self.customers[2]
         can_buy_pet = customer_can_afford_pet(customer, self.new_pet)
         self.assertEqual(True, can_buy_pet)
 
-# 21.
+# 21, 22, 23
     # These are 'integration' tests so we want multiple asserts.
     # If one fails the entire test should fail
     #
@@ -215,7 +211,6 @@ class TestPetShop(unittest.TestCase):
         self.assertEqual(100, get_customer_cash(customer))
         self.assertEqual(1900, get_total_cash(self.cc_pet_shop))
 
-# 22.
     @unittest.skip("delete this line to run the test")
     def test_sell_pet_to_customer__pet_not_found(self):
         customer = self.customers[0]
@@ -228,7 +223,6 @@ class TestPetShop(unittest.TestCase):
         self.assertEqual(1000, get_customer_cash(customer))
         self.assertEqual(1000, get_total_cash(self.cc_pet_shop))
 
-# 23.
     @unittest.skip("delete this line to run the test")
     def test_sell_pet_to_customer__insufficient_funds(self):
         customer = self.customers[1]
